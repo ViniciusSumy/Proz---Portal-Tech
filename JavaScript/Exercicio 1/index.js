09cd0b0e2ad4dbdbaa32cb296131ca68fcc6e584
Vinicius Marques <marquess.vini@gmail.com>
2022-12-13 14:08:29 -0300

    Criado Exercicio 2 de JavaScript

---
+++ JavaScript/Exercicio 1/index.js
@@ -0,0 +1,11 @@
+const nomeUsuario = 'Vinicius'
+let presenteNatal = 'Bicicleta'
+let comportamento = true
+
+comportamento = false
+
+if ( comportamento = true ) {
+  console.log (`${nomeUsuario} se comportou bem esse ano e vai ganhar uma ${presenteNatal}`)
+} else if ( comportamento = false ) {
+  console.log ('Infelizmente sem presentes esse ano, se comporte melhor!')
+}
