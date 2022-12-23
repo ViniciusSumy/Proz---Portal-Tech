4a0e0d2bbdbc9b1d14e64d78540459aa75fe29f1
Vinicius Marques <marquess.vini@gmail.com>
2022-12-22 21:03:41 -0300

    Inclusão de onclick e função de remover o visible.

--- JavaScript/Exercício 8/script.js
+++ JavaScript/Exercício 8/script.js
@@ -3,15 +3,20 @@ const error_text = document.getElementsByClassName('error-text');
 const username_input = document.getElementById('login-usuario');
 const password_input = document.getElementById('login-senha');
 
-title[0].style.color = 'red';
+title[0].style.color = 'purple';
 
 function validarForms () {
     if (username_input.value != 'viniciusmarques') {
         error_text[0].classList.add ('visible')
     }
-
+    else {
+    error_text[0].classList.remove ('visible')
+    }
     if (password_input.value != 'senha') {
         error_text[1].classList.add ('visible')
+    }
+    else {
+        error_text[1].classList.remove ('visible')
     }
 }
 
