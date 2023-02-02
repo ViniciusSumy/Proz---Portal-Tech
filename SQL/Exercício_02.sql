61ef0f0d9253945e0aeb9d23cb34bbb0eb75670e
Vinicius Marques <marquess.vini@gmail.com>
2023-02-01 22:48:38 -0300

    Atualização no exercício 2

--- SQL/Exercício_02.sql
+++ SQL/Exercício_02.sql
@@ -1,5 +1,10 @@
-Create table escola(
-    id serial primary key,
+Create table alunos(
+    aluno_id serial primary key,
     nome VARCHAR(36) NOT NULL,
     email VARCHAR NOT NULL,
     endereco VARCHAR NOT NULL);
+
+insert into escola(nome,email,endereco)values
+    ('João','joao@email.com', 'Rua das Ruas, 142');
+
+    select * from alunos;
