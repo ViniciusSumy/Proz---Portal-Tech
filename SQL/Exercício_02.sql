0a2a8465a8a9c06427cb21e8f5ea1cf75dac5a8d
Vinicius Marques <marquess.vini@gmail.com>
2023-02-01 22:49:41 -0300

    Incluido detalhes da atividade

--- SQL/Exercício_02.sql
+++ SQL/Exercício_02.sql
@@ -1,3 +1,11 @@
+-- 1. crie uma tabela chamada ALUNO;  
+-- 2. defina os atributos da tabela;
+-- 3. adicione a chave primária de nome ID (identificador);
+-- 4. adicione um atributo nome do tipo varchar;
+-- 5. adicione um atributo e-mail do tipo varchar;
+-- 6. adicione um atributo endereço do tipo varchar.
+
+
 Create table alunos(
     aluno_id serial primary key,
     nome VARCHAR(36) NOT NULL,
